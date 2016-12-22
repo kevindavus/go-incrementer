845498dd74ce071a24902dc548f4c0d9677de474
Kevin Davis <osfan501@gmail.com>
2016-12-22 10:59:40 -0500

    uploading go

---
+++ increment/stress.sh
@@ -0,0 +1,350 @@
+curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"curl -X POST http://localhost:3333/increment -d 'key=abcde&value=524245' &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=7525" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=3" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=6" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+curl -X POST http://localhost:3333/increment -d "key=abcde&value=5" &
+pidlist="$pidlist $!"
+
+for job in $pidlist do
+  echo $job
+  wait $job || let "FAIL+=1"
+done
+
+if [ "$FAIL" == "0" ]; then
+  echo "YAY!"
+else
+  echo "FAIL! ($FAIL)"
+fi
